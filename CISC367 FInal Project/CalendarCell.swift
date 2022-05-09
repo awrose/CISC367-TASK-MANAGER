fa3f70b34b3fd3b53cbe4d353da08c0243cf7598
awrose <awrose@udel.edu>
2022-05-09 18:47:49 -0400

    full monthly calendar view, ability to go to next and previous months

---
+++ CISC367 FInal Project/CalendarCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCell.swift
+//  CISC367 FInal Project
+//
+//  Created by Abigail Walters on 5/9/22.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
---
+++ CISC367 FInal Project/CalendarHelper.swift
@@ -0,0 +1,58 @@
+//
+//  CalendarHelper.swift
+//  CISC367 FInal Project
+//
+//  Created by Abigail Walters on 5/9/22.
+//
+
+import Foundation
+import UIKit
+
+class CalendarHelper{
+    let calendar = Calendar.current
+    
+    func plusMonth(date: Date) -> Date{
+        //will go to the next month
+        return calendar.date(byAdding: .month, value: 1, to: date)!
+    }
+    
+    func minusMonth(date: Date) -> Date{
+        //will go to the previous month
+        return calendar.date(byAdding: .month, value: -1, to: date)!
+    }
+    
+    func monthString(date: Date) -> String {
+        //formats the month string for label
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        return dateFormatter.string(from: date)
+    }
+    
+    func yearString(date: Date) -> String {
+        //formats the year for label
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        return dateFormatter.string(from: date)
+    }
+    
+    func daysInMonth(date: Date) -> Int{
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        return range.count
+    }
+    
+    func dayOfMonth(date: Date) -> Int{
+        let components = calendar.dateComponents([.day], from: date)
+        return components.day!
+    }
+    
+    func firstOfMonth(date: Date) -> Date{
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return calendar.date(from: components)!
+    }
+    
+    func weekDay(date: Date) -> Int{
+        let components = calendar.dateComponents([.weekday], from: date)
+        return components.weekday! - 1
+    }
+    
+}
